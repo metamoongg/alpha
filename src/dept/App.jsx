adef56a78032d7e10cc92de4be8132f2cad6aa0f
silvia40 <silvia40@naver.com>
2023-10-26 15:12:31 +0900

    DB

--- src/dept/App.jsx
+++ src/dept/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import {sprintf} from 'sprintf-js'
 import sleep from 'es7-sleep'
 import List from "./List.jsx"
-
+import "./App.css"
 
 class App extends React.Component {
-
+	
 	render() {
 		return (
 			<Routes>
-				<Route path='/rest/dept' element={<List/>}/>
+				<Route path='/rest/dept' element={<List/>} />
 			</Routes>
 		)
 	}
@@ -19,7 +19,7 @@ class App extends React.Component {
 
 let root = ReactDOM.createRoot(document.querySelector("#root"))
 root.render(
-	<BrowserRouter>	
+	<BrowserRouter>
 		<App/>
 	</BrowserRouter>
 )
--- src/dept/List.jsx
+++ src/dept/List.jsx
@@ -8,19 +8,52 @@ class List extends React.Component {
 		super();
 		
 		this.init();
+		console.log("constructor...");
 	}
 	
 	async init() {
 		let response = await fetch("/rest/dept")
 		let json = await response.json();
 		
-		console.log(json)
+		this.state.data = json.data;
+		
+		console.log(this.state.data);
+		this.forceUpdate();
+	}
+	
+	state = {
+		data: []
 	}
 	
 	render() {
 		return (
 			<>
-			<h1>부서목록</h1>
+			<h1>부서 목록</h1>
+			<hr/>
+			<table border={1}>
+				<thead>
+					<tr>
+					<th>deptno</th>
+					<th>dname</th>
+					<th>loc</th>
+					<th>수정</th>
+					<th>삭제</th>
+					</tr>
+				</thead>
+				<tbody>
+				{
+					this.state.data.map(dept =>
+						<tr key={dept.deptno}>
+							<td>{dept.deptno}</td>
+							<td>{dept.dname}</td>
+							<td>{dept.loc}</td>
+							<td>Update</td>
+							<td>Delete</td>
+						</tr>
+					)
+				}
+				</tbody>
+			</table>
 			</>
 		)
 	}
